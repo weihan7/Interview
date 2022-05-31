2fd05e4e53ec1363d5f047e57cdd48f0eee06ab1
weihan7 <18810499157@163.com>
2022-05-31 16:44:26 +0800

    update: crawler

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,5 +1,5 @@
-import http from "http"         // 提示：可选依赖库
-import axios from "axios"       // 提示：可选依赖库
+// import http from "http"         // 提示：可选依赖库
+import axios from "axios"      // 提示：可选依赖库
 
 function getUrl(i: number): string {
     let prefix: String = "http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/"
@@ -7,32 +7,55 @@ function getUrl(i: number): string {
     return prefix.toString() + i.toString() + suffix.toString()
 }
 
-function getNum(url: string): Promise<void> {
-    // input your code 
+async function getNum() {
+    let numArr = [];
+    for (let i = 1; i < 11; i++) {
+        let url = getUrl(i);
+        const result = await axios.get(url);
+        if (result.data && result.status === 200) {
+            numArr.push(result.data.match(/\d+/g));
+        } else {
+          new Error(result.data.msg);
+        } 
+    }
+    console.log(numArr);
+    let newArr = flatten(numArr);
+    console.log(newArr);
+    let result = newArr.reduce((x, y) => {
+        return x + y
+    })
+    console.log(result);
+    return result;
 }
 
-function regexpFromat(str: string): RegExpMatchArray | null {
- // input your code 
+function flatten(array: number[]) {
+    return array.toString().split(',').map((item)=>{
+        return Number(item);
+    })
 }
+getNum()
 
-function syncExecute(): void {
-    // input your code 
+// function regexpFromat(str: string): RegExpMatchArray | null {
+//  // input your code 
+// }
 
-}
+// function syncExecute(): void {
+//     // input your code 
+// }
 
-async function asyncExecute(): Promise<number | string> {
-   // input your code 
+// async function asyncExecute(): Promise<number | string> {
+//    // input your code 
 
-}
+// }
 
-async function main(): Promise<number | string> {
-    await asyncExecute()
-    await console.log("------------------")
-    await console.log("------------------")
-    await syncExecute()
-}
+// async function main(): Promise<number | string> {
+//     await asyncExecute()
+//     await console.log("------------------")
+//     await console.log("------------------")
+//     await syncExecute()
+// }
 
-main()
+// main()
 
 
 
