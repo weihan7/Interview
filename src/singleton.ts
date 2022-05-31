95dd4ed78e92f3088d1abb47b6d34002efaa52c8
weihan7 <18810499157@163.com>
2022-05-31 11:40:59 +0800

    update: singleton模式

--- src/singleton.ts
+++ src/singleton.ts
@@ -1,19 +1,27 @@
 class LazySingleton {
     // 实例作为静态的私有属性
     private static instance: LazySingleton
-    public name: string
+    public name!: string
 
-    public static getInstance(name):LazySingleton{
+    //懒汉式可能存在线程安全问题
+    public static getInstance(name:string):LazySingleton{
         // input your code 
-        return     
+        console.log(name);
+        if (!this.instance) {
+            this.instance = new LazySingleton();
+        }
+        return this.instance;
     }
 }
 class HungrySingleton {
     // 实例作为静态的私有属性
     private static instance: HungrySingleton = new HungrySingleton()
+    public name!: string
 
+    //饿汉式开始就创建好，防止线程安全问题，建议使用
     public static  getInstance(): HungrySingleton{
         // input your code 
+        return this.instance;
     }
 }
 
@@ -21,7 +29,7 @@ class HungrySingleton {
 let lazy_single = LazySingleton.getInstance('LazySingleton')
 console.log(lazy_single.name)
 
-let lazy_single2 = LazySingleton.getInstance()
+let lazy_single2 = LazySingleton.getInstance('LazySingleton2')
 console.log(lazy_single === lazy_single2)
 
 lazy_single.name = 'test'
